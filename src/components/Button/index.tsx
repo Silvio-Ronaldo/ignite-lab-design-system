a68af419b63510d044e6a8f692810182988a490e
Silvio-Ronaldo <silvio.ronaldo77@gmail.com>
2022-10-14 02:54:15 -0300

    feat: create full interface with components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,20 +1,22 @@
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { clsx } from 'clsx';
 import { Slot } from '@radix-ui/react-slot';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
-    asChild: boolean;
+    asChild?: boolean;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, className, ...props }: ButtonProps) {
     const Comp = asChild ? Slot : 'button';
 
     return (
         <Comp 
             className={clsx(
                 "w-full bg-cyan-500 py-3 px-4 font-sans text-sm text-black font-semibold rounded transition-colors hover:bg-cyan-300 focus:ring-2 ring-white",
+                className
             )}
+            {...props}
         >
             { children }
         </Comp>
